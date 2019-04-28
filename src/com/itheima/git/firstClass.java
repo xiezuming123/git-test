b2ec16729862e53cd2437f202d0af1c53f6598b6
xiezuming <412061556@qq.com>
2019-04-28 23:13:50 +0800

    把本地工厂添加

---
+++ src/com/itheima/git/firstClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+        System.out.println("hello.git");
+    }
+}
